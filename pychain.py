c526fcb32972a9c67733d634692bb903ac138fac
MaxAcheson <maxacheson@gmail.com>
2022-04-05 17:51:56 -0700

    Added user ID column back after an error

--- pychain.py
+++ pychain.py
@@ -69,6 +69,7 @@ class Block:
     # Rename the `data` attribute to `record`, and set the data type to `Record`
     record: Record
 
+    creator_id: int
     prev_hash: str = "0"
     timestamp: str = datetime.datetime.utcnow().strftime("%H:%M:%S")
     nonce: int = 0
